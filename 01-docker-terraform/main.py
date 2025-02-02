125620862b324c21f2b03ee393d15eae702bb494
unknown <yonkimkosal@gmail.com>
2025-02-02 13:46:54 +0700

    Changes To Folder Structure

---
+++ 01-docker-terraform/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# trip_data = pd.read_csv()
+
+trip_data_path = 'data/green_tripdata_2019-10.csv'
+
+trip_data = pd.read_csv(trip_data_path)
--- main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# trip_data = pd.read_csv()
-
-trip_data_path = 'data/green_tripdata_2019-10.csv'
-
-trip_data = pd.read_csv(trip_data_path)
